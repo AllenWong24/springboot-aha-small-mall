d98b42400a83865cb48f0abc124fb63db8623d9b
Allen Wong <allenwong2407@outlook.com>
2020-05-30 01:14:40 +0800

    项目搭建

---
+++ src/test/java/com/aha/smallmall/SmallmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aha.smallmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
